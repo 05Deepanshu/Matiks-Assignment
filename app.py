f08b58c6d66e57362128976b636f45db4f5916fd
deepanshukatariya450@gmail.com <94599793+05Deepanshu@users.noreply.github.com>
2025-07-04 14:12:15 +0530

    correct the indentation

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="User Behaviour Dashboard", layout="wide")
-st.title("Matiks User Behaviour Dashboard")
+st.set_page_config(page_title="User Behavior Dashboard", layout="wide")
+st.title("📊 Matiks User Behavior Dashboard")
 
-df = pd.read_csv("Matiks - Data Analyst Data - Sheet1.csv"), parse_dates = ["session_date"])
+df = pd.read_csv("Matiks - Data Analyst Data - Sheet1.csv",
+                 parse_dates=['session_date'])
 
-    st.subheader("Raw Data")
-    st.dataframe(df.head())
+st.subheader("Raw Data")
+st.dataframe(df.head())
 
-    st.subheader("Daily Active Users (DAU)")
-    dau = df.groupby("session_date")["user_id"].nunique().reset_index()
-    fig = px.line(dau, x="session_date", y="user_id", title="Daily Active Users")
-    st.plotly_chart(fig, use_container_width=True)
+st.subheader("📅 Daily Active Users (DAU)")
+dau = df.groupby("session_date")["user_id"].nunique().reset_index()
+fig = px.line(dau, x="session_date", y="user_id", title="Daily Active Users")
+st.plotly_chart(fig, use_container_width=True)
