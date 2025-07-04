4deff2fb9a01227823b91778172272196ac04d7b
deepanshukatariya450@gmail.com <94599793+05Deepanshu@users.noreply.github.com>
2025-07-04 14:12:43 +0530

    ..

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(page_title="User Behavior Dashboard", layout="wide")
-st.title("📊 Matiks User Behavior Dashboard")
+st.title("Matiks User Behavior Dashboard")
 
 df = pd.read_csv("Matiks - Data Analyst Data - Sheet1.csv",
                  parse_dates=['session_date'])
@@ -11,7 +11,7 @@ df = pd.read_csv("Matiks - Data Analyst Data - Sheet1.csv",
 st.subheader("Raw Data")
 st.dataframe(df.head())
 
-st.subheader("📅 Daily Active Users (DAU)")
+st.subheader("Daily Active Users (DAU)")
 dau = df.groupby("session_date")["user_id"].nunique().reset_index()
 fig = px.line(dau, x="session_date", y="user_id", title="Daily Active Users")
 st.plotly_chart(fig, use_container_width=True)
